6bec8efa5e91f67d6caba8a905db92a946c9d065
IbrahimQumseya <52652965+IbrahimQumseya@users.noreply.github.com>
2022-03-22 16:45:15 +0200

    fixing the env problem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -31,13 +31,22 @@ module.exports = {
       script: 'dist/main.js',
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      args: 'one two',
+      args: 'start',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env: {
-        NODE_ENV: 'production',
+        PORT: 3000,
+        NODE_ENV: 'development',
+        DB_HOST: 'localhost',
+        DB_PORT: 5432,
+        DB_USERNAME: 'postgres',
+        DB_PASSWORD: 'postgres',
+        DB_DATABASE: 'task-management',
+        STAGE: 'dev',
+        JWT_SECRECT:
+          'FrJBWSh^N$Lwj59?eTbC=La2hLwF3BN!F+D5bFD7+#$FPSCqp?wYh@vLd+HKTF%ZR#urx?dbPjcKkMdeRDjj$cw&RYD5Gk2KYhp7B4gu*fhZ=Q&xL#DwLeN%RXPudTFUw5++e68emrv$!n24N_gd5vMkD%YEk3E7X4_gwtY+b?DPty-^PfUcBkDwxWF!tR8+-7*p4V4euWRQLZH8#4KLeah9eDtA^^+DjU#jAh5rjR$g*#!hatqCkbZ^2@-ku%^w',
       },
     },
   ],
